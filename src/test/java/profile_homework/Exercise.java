daa5f5629d58cc2733192afac065e7f339ec3e89
Evelin Arnaudova <evelin.arnaudova@gmail.com>
2023-01-29 09:47:09 +0200

    sign in and sign up methods //needs editing

--- src/test/java/profile_homework/Exercise.java
+++ src/test/java/profile_homework/Exercise.java
@@ -1,4 +1,152 @@
 package profile_homework;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+import java.time.Duration;
 
 public class Exercise {
+    public static final String PASSWORD = "Password1.1Drowssap";
+    private WebDriver driver;
+
+    @BeforeSuite
+    protected final void setupTestSuite() {
+        WebDriverManager.chromedriver().setup();
+        WebDriverManager.firefoxdriver().setup();
+        WebDriverManager.edgedriver().setup();
+    }
+
+    @BeforeMethod
+    protected final void setUpTest() {
+        this.driver = new ChromeDriver();
+        this.driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+    }
+
+    @AfterMethod
+    protected final void tearDownTest() {
+        if (this.driver != null) {
+            this.driver.close();
+        }
+    }
+
+    @Test
+    public void testRegistration() {
+        driver.get("http://training.skillo-bg.com:4300/posts/all");
+        WebElement loginLink = driver.findElement(By.id("nav-link-login"));
+        loginLink.click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.urlToBe("http://training.skillo-bg.com:4300/users/login"));
+
+        WebElement signInElement = driver.findElement(By.xpath("//*[text()='Sign in']"));
+        wait.until(ExpectedConditions.visibilityOf(signInElement));
+
+        WebElement registerLink = driver.findElement(By.linkText("Register"));
+        registerLink.click();
+
+        wait.until(ExpectedConditions.urlToBe("http://training.skillo-bg.com:4300/users/register"));
+
+        WebElement signUpElement = driver.findElement(By.xpath("//*[text()='Sign up']"));
+        wait.until(ExpectedConditions.visibilityOf(signUpElement));
+
+        WebElement userNameField = driver.findElement(By.name("username"));
+        String username = generateRandomAlphabeticString(5, 8);
+        userNameField.sendKeys(username);
+
+        WebElement emailField = driver.findElement(By.cssSelector("[type='email']"));
+        String email = generateRandomEmail(5, 10);
+        emailField.sendKeys(email);
+
+        WebElement birthDateElement = driver.findElement(By.cssSelector("[formcontrolname='birthDate']"));
+        birthDateElement.sendKeys("11122003");
+
+        WebElement passwordField = driver.findElement(By.id("defaultRegisterFormPassword"));
+        passwordField.sendKeys(PASSWORD);
+
+        WebElement confirmPasswordField = driver.findElement(By.id("defaultRegisterPhonePassword"));
+        confirmPasswordField.sendKeys(PASSWORD);
+
+        WebElement infoField = driver.findElement(By.name("pulic-info"));
+        String info = generateRandomAlphabeticString(10, 15);
+        infoField.sendKeys(info);
+
+        WebElement signInButton = wait.until(ExpectedConditions.elementToBeClickable(By.id("sign-in-button")));
+        signInButton.click();
+
+        wait.until(ExpectedConditions.urlToBe("http://training.skillo-bg.com:4300/posts/all"));
+
+        WebElement profileLink = wait.until(ExpectedConditions.elementToBeClickable(By.id("nav-link-profile")));
+        profileLink.click();
+
+        wait = new WebDriverWait(driver, Duration.ofSeconds(25));
+        wait.until(ExpectedConditions.urlContains("http://training.skillo-bg.com:4300/users/"));
+
+        Boolean isTextDisplayed = wait.until(ExpectedConditions.textToBe(By.tagName("h2"), username));
+        Assert.assertTrue(isTextDisplayed, "The username is not displayed!");
+    }
+
+    private String generateRandomEmail(int minLengthInclusive, int maxLengthInclusive) {
+        return generateRandomAlphabeticString(minLengthInclusive, maxLengthInclusive) + "@gmail.com";
+    }
+
+    private String generateRandomAlphabeticString(int minLengthInclusive, int maxLengthInclusive) {
+        return RandomStringUtils.randomAlphanumeric(minLengthInclusive, maxLengthInclusive);
+    }
+
+    @DataProvider(name = "generateUsers")
+    public Object[][] generateUsers(){
+        return new Object[][]{
+                {"earnaudova@gmail.com", "test123", "earnaudova" }, //login with email
+                {"DimitarTarkalanov", "Dimitar1.Tarkalanov1", "DimitarTarkalanov"},//login with username
+                {"testAdmin@gmail.com", "Admin1.User1", "AdminUser"}, //login with admin user
+                {"manager@gmail.com", "Manager1.Use1", "ManagerUser"} //login with manager user
+        };
+    }
+
+    @Test(dataProvider = "generateUsers")
+    public void testLogin(String user, String pass, String name){
+
+        driver.get("http://training.skillo-bg.com:4300/posts/all");
+
+        WebElement loginButton = driver.findElement(By.id("nav-link-login"));
+        loginButton.click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.urlToBe("http://training.skillo-bg.com:4300/users/login"));
+
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement signInTitle = driver.findElement(By.xpath("//p[text()='Sign in']"));
+        wait.until(ExpectedConditions.visibilityOf(signInTitle));
+
+        WebElement username = driver.findElement(By.id("defaultLoginFormUsername"));
+        username.sendKeys(user);
+
+        WebElement password = driver.findElement(By.id("defaultLoginFormPassword"));
+        password.sendKeys(pass);
+
+        WebElement signInButton = driver.findElement(By.id("sign-in-button"));
+        wait.until(ExpectedConditions.elementToBeClickable(signInButton));
+        signInButton.click();
+
+        WebElement profileLink = driver.findElement(By.id("nav-link-profile"));
+        wait.until(ExpectedConditions.elementToBeClickable(profileLink));
+        profileLink.click();
+
+        wait.until(ExpectedConditions.urlContains("http://training.skillo-bg.com:4300/users/"));
+
+        Boolean isTextDisplayed = wait.until(ExpectedConditions.textToBe(By.tagName("h2"), name));
+        Assert.assertTrue(isTextDisplayed);
+
+    }
+
 }
