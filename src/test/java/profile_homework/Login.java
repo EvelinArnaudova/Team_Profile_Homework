03c32578ecb5c4bf290356a4e567b1f03fc936c2
Evelin Arnaudova <evelin.arnaudova@gmail.com>
2023-01-29 13:14:05 +0200

    adding classes and set up for parallel testing

--- src/test/java/profile_homework/Login.java
+++ src/test/java/profile_homework/Login.java
@@ -1,11 +1,9 @@
 package profile_homework;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -13,9 +11,8 @@ import org.testng.annotations.*;
 
 import java.time.Duration;
 
-public class Exercise {
-    public static final String PASSWORD = "Password1.1Drowssap";
-    private WebDriver driver;
+public class Login {
+    public WebDriver driver;
 
     @BeforeSuite
     protected final void setupTestSuite() {
@@ -38,70 +35,6 @@ public class Exercise {
             this.driver.close();
         }
     }
-
-   /* @Test(priority = 0)
-    public void testRegistration() {
-        driver.get("http://training.skillo-bg.com:4300/posts/all");
-        WebElement loginLink = driver.findElement(By.id("nav-link-login"));
-        loginLink.click();
-
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.urlToBe("http://training.skillo-bg.com:4300/users/login"));
-
-        WebElement signInElement = driver.findElement(By.xpath("//*[text()='Sign in']"));
-        wait.until(ExpectedConditions.visibilityOf(signInElement));
-
-        WebElement registerLink = driver.findElement(By.linkText("Register"));
-        registerLink.click();
-
-        wait.until(ExpectedConditions.urlToBe("http://training.skillo-bg.com:4300/users/register"));
-
-        WebElement signUpElement = driver.findElement(By.xpath("//*[text()='Sign up']"));
-        wait.until(ExpectedConditions.visibilityOf(signUpElement));
-
-        WebElement userNameField = driver.findElement(By.name("username"));
-        String username = generateRandomAlphabeticString(5, 8);
-        userNameField.sendKeys(username);
-
-        WebElement emailField = driver.findElement(By.cssSelector("[type='email']"));
-        String email = generateRandomEmail(5, 10);
-        emailField.sendKeys(email);
-
-        WebElement birthDateElement = driver.findElement(By.cssSelector("[formcontrolname='birthDate']"));
-        birthDateElement.sendKeys("11011990");
-
-        WebElement passwordField = driver.findElement(By.id("defaultRegisterFormPassword"));
-        passwordField.sendKeys(PASSWORD);
-
-        WebElement confirmPasswordField = driver.findElement(By.id("defaultRegisterPhonePassword"));
-        confirmPasswordField.sendKeys(PASSWORD);
-
-        WebElement infoField = driver.findElement(By.name("pulic-info"));
-        String info = generateRandomAlphabeticString(10, 15);
-        infoField.sendKeys(info);
-
-        WebElement signInButton = wait.until(ExpectedConditions.elementToBeClickable(By.id("sign-in-button")));
-        signInButton.click();
-
-        wait.until(ExpectedConditions.urlToBe("http://training.skillo-bg.com:4300/posts/all"));
-
-        WebElement profileLink = wait.until(ExpectedConditions.elementToBeClickable(By.id("nav-link-profile")));
-        profileLink.click();
-
-        wait = new WebDriverWait(driver, Duration.ofSeconds(25));
-        wait.until(ExpectedConditions.urlContains("http://training.skillo-bg.com:4300/users/"));
-
-        Boolean isTextDisplayed = wait.until(ExpectedConditions.textToBe(By.tagName("h2"), username));
-        Assert.assertTrue(isTextDisplayed, "The username is not displayed!");
-    }
-
-    private String generateRandomEmail(int minLengthInclusive, int maxLengthInclusive) {
-        return generateRandomAlphabeticString(minLengthInclusive, maxLengthInclusive) + "@gmail.com";
-    }
-
-    private String generateRandomAlphabeticString(int minLengthInclusive, int maxLengthInclusive) {
-        return RandomStringUtils.randomAlphanumeric(minLengthInclusive, maxLengthInclusive);
-    }*/
 
     @DataProvider(name = "generateUsers")
     public Object[][] generateUsers(){
@@ -144,7 +77,7 @@ public class Exercise {
         Boolean isTextDisplayed = wait.until(ExpectedConditions.textToBe(By.tagName("h2"), name));
         Assert.assertTrue(isTextDisplayed);
 
-        WebElement modifyProfile = driver.findElement(By.xpath("//h2[text()='TestSkillo123456']/following-sibling::i"));
+        /*WebElement modifyProfile = driver.findElement(By.xpath("//h2[text()='TestSkillo123456']/following-sibling::i"));
         wait.until(ExpectedConditions.elementToBeClickable(modifyProfile));
         modifyProfile.click();
 
@@ -165,7 +98,7 @@ public class Exercise {
 
         WebElement saveButton = driver.findElement(By.xpath("//button[@type=\"submit\"]"));
         wait.until(ExpectedConditions.elementToBeClickable(saveButton));
-        saveButton.click();
+        saveButton.click();*/
 
     }
 
